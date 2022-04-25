07fad4df4ffdd6fc20e6951e9fee1530ea26f7ff
Abi <abirami.22@ichat.sp.edu.sg>
2022-04-25 10:50:14 +0800

    Lab2.0

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,24 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+start = str(input("Enter some numbers separated by commas (e.g. 5, 67, 32)"))
+#Num1 = start.split(",")[0]
+#Num2 = start.split(",")[1]
+#Num3 = start.split(",")[2]
+Numbers = int(str(start))
+
+print (start)
+print (Numbers)
+#Numb1 = int(Num1)
+#Numb2 = int(Num2)
+#Numb3 = int(Num3) kadfkadfkja
+
+#Maximum = [Numb1, Numb2, Numb3]
+##Bmax = max(Maximum)
+##print(Bmax)
+
+
+
+
+
+
+
+
